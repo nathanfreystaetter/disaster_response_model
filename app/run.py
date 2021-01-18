cc8518b8c9a563fcd9101e8ce91d5b966edfd811
Nathan Freystaetter <nfreystaetter@gmail.com>
2021-01-18 16:06:13 -0500

    fixed error in SQL database pull

--- app/run.py
+++ app/run.py
@@ -27,7 +27,7 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('disaster', engine)
+df = pd.read_sql_table('factMessages', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
